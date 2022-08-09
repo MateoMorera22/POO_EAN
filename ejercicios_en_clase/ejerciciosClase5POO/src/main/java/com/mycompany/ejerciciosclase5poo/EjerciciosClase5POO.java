2b56494fefc634848e6e7865d56a24edaacee96b
Mateo Morera <56879042+MateoMorera22@users.noreply.github.com>
2022-08-09 18:29:39 -0500

    Se agregan apuntes de clase

---
+++ ejercicios_en_clase/ejerciciosClase5POO/src/main/java/com/mycompany/ejerciciosclase5poo/EjerciciosClase5POO.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.ejerciciosclase5poo;
+
+/**
+ *
+ * @author SALA
+ */
+public class EjerciciosClase5POO {
+
+     // declarando otra funcion
+    static int sumar(int a, int b){
+        int resultado = (a+b);
+        return resultado;
+    }
+    
+    
+    public static void main(String[] args) {
+       int a = 2;
+       int b = 3;
+        System.out.println(sumar(a, b));
+        
+    }
+}
